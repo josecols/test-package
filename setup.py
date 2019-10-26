1d58998fcb7b14312e4935bfa14a5efaf3b2cd9d
José Cols <josecolsg@gmail.com>
2019-10-25 23:47:36 -0500

    Updated version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gcf_ndb_models',
-      version='0.1',
+      version='0.0.1',
       description='GCF ndb model package',
       url='https://github.com/josecols/test-package',
       author='Jose Cols',
