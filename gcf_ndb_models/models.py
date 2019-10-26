52d91a3e99a1ef69078d879420d00897db63ee08
José Cols <josecolsg@gmail.com>
2019-10-26 01:33:59 -0500

    Added context to models.

--- gcf_ndb_models/models.py
+++ gcf_ndb_models/models.py
@@ -1,5 +1,7 @@
 from google.cloud import ndb
 
+client = ndb.Client()
+
 
 class Contact(ndb.Model):
     name = ndb.StringProperty()
@@ -8,18 +10,20 @@ class Contact(ndb.Model):
 
     @classmethod
     def get_by_name(cls, name='Model'):
-        return cls.query(name == name).get()
+        with client.context():
+            return cls.query(name == name).get()
 
     @classmethod
     def create_or_update(cls, data):
-        if data.get('id'):
-            contact = cls.get_by_id(int(data.get('id')))
-        else:
-            contact = cls()
+        with client.context():
+            if data.get('id'):
+                contact = cls.get_by_id(int(data.get('id')))
+            else:
+                contact = cls()
 
-        contact.name = data.get('name')
-        contact.phone = data.get('phone')
-        contact.email = data.get('email')
-        contact.put()
+            contact.name = data.get('name')
+            contact.phone = data.get('phone')
+            contact.email = data.get('email')
+            contact.put()
 
-        return contact
+            return contact.to_dict()
