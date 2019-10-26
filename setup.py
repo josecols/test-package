b305f4d7d37d5b2d2fd0a48db526531e2b04cc5c
José Cols <josecolsg@gmail.com>
2019-10-26 00:23:59 -0500

    Updated setup requirements.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='gcf_ndb_models',
-      version='0.0.1',
-      description='GCF ndb model package',
-      url='https://github.com/josecols/test-package',
-      author='Jose Cols',
-      author_email='josecolsg@gmail.com',
-      license='MIT',
-      packages=['gcf_ndb_models'],
-      zip_safe=False)
+setup(
+    name='gcf_ndb_models',
+    version='0.0.1',
+    description='GCF ndb model package',
+    url='https://github.com/josecols/test-package',
+    author='Jose Cols',
+    author_email='josecolsg@gmail.com',
+    license='MIT',
+    packages=['gcf_ndb_models'],
+    zip_safe=False,
+    install_requires=['google-cloud-ndb']
+)
